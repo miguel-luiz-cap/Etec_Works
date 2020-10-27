53801293d88097c6b8e3c37802a01257e4126aad
Miguel Luiz Capristano <71992751+miguel-luiz-cap@users.noreply.github.com>
2020-10-26 22:23:44 -0300

    Create DateTimeExemplo.cs

---
+++ 1 Semeste/Algoritimo/C#/DateTimeExemplo.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemplo1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			DateTime hoje;
+			hoje = DateTime.Now;
+			Console.WriteLine(hoje);
+			Console.ReadKey();
+		}
+	}
+}
